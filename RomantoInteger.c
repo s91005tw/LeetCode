da02c77b47123a679142afa6e2da9a8757d497d3
AllenZhong <AllenZhong@ami.com>
2023-03-29 19:32:14 +0800

    can printf Hundreds, Thousands

--- RomantoInteger.c
+++ RomantoInteger.c
@@ -2,8 +2,10 @@
 #include<stdlib.h>
 
 int romanToInt(char * s){
-    int i;
+    int i, j, Result;
+    int Digits, Tens, Hundreds, Thousands;
 
+    Result = 0;
     for(i=0; ; i++){
         if(s[i] == '\0' ){
             break;
@@ -12,16 +14,96 @@ int romanToInt(char * s){
     }
     printf("\n");
 
-    return 1;
+    for(i=0; ; i++){
+        if(s[i] == '\0' ){
+            break;
+        }
+        if(s[i] == 'M'){
+            if(i==0){
+                Result = Result + 1000;
+                printf("Result = %d\n", Result);
+            }
+            else{
+                if(s[i-1] == 'M'){
+                    Result = Result + 1000;
+                    printf("Result = %d\n", Result);
+                }
+                else{
+                    Result = Result + 1000;
+
+                    if(s[i-1] == 'D'){
+                        Result = Result - 500;
+                    }
+                    else{
+                        j = i-1;
+                        while(j>=0){
+                            if(s[j] == 'C'){
+                                Result = Result - 100;
+                            }
+                            j--;
+                        }
+                    }
+
+                }
+            }
+        }
+        if(s[i] == 'D'){
+            if(i==0){
+                Result = Result + 500;
+                printf("Result = %d\n", Result);
+
+                j=i+1;
+                while(s[j] != '\0'){
+                    if(s[j] == 'C'){
+                        Result = Result + 100;
+                        printf("Result = %d\n", Result);
+                    }
+                    j++;
+                }
+            }
+            else{
+                Result = Result + 500;
+                printf("Result = %d\n", Result);
+                j = i-1;
+                while(j>=0){
+                    if(s[j] == 'C'){
+                        Result = Result - 100;
+                        printf("Result = %d\n", Result);
+                    }
+                    j--;
+                }
+
+                j=i+1;
+                while(s[j] != '\0'){
+                    if(s[j] == 'C'){
+                        Result = Result + 100;
+                        printf("Result = %d\n", Result);
+                    }
+                    j++;
+                }
+            }
+        }
+        if(s[i] == 'C'){
+            if(i==0){
+                Result = Result + 100;
+                printf("Result = %d\n", Result);
+
+            }
+
+
+        }
+    }
+
+
+    return Result;
 
 }
 
 
 int main(){
     int Numerals;
-    char RomanNumerals[] = "vix";
-    printf("test\n");
-    //RomanNumerals[] = "vix";
+    char RomanNumerals[] = "CCCD";
+    //printf("test\n");
 
     Numerals = romanToInt(RomanNumerals);
 
